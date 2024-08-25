ede76f1db16e300d06472f61b415d5ac521f3cdd
solypsismo <nomec505@gmail.com>
2024-08-26 01:38:29 +0200

    get url param

--- client/src/routes/home/[roomID]/+page.svelte
+++ client/src/routes/home/[roomID]/+page.svelte
@@ -1,5 +1,9 @@
 <script>
+    import { page } from '$app/stores';
+    
     export let data
+
+    let roomID = $page.params.roomID
 </script>
 
-ciao {data.user.username}
+ciao {data.user.username} nella stanza {roomID}
